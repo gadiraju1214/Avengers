ae7e92897be8d9ebaa36bdcb1f5d382299570224
kulbir <kulbirboparai97@gmail.com>
2016-08-31 23:26:10 +1000

    code review

--- ITC515_assignment2/src/datamanagement/IStudentUnitRecord.java
+++ ITC515_assignment2/src/datamanagement/IStudentUnitRecord.java
@@ -1,18 +1,32 @@
 package datamanagement;
 
-public interface IStudentUnitRecord {
+public interface IStudentUnitRecord { 
+	// an interface class for students unit record
 
-    public Integer getStudentID();
-    public String getUnitCode();
+	public Integer getStudentID();
+	//method to get student id
 
-    public void setAsg1(float mark);
-    public float getAsg1();
+	public String getUnitCode();
+	//method to get code of subject
 
-    public void setAsg2(float mark);
-    public float getAsg2();
+	public void setAsg1(float mark);
+	//set marks of assignment1 
 
-    public void setExam(float mark);
-    public float getExam();
+	public float getAsg1();
+	//get method to get marks of assignment 2
 
-    public float getTotal();
+	public void setAsg2(float mark);
+	//set marks of assignment1 
+
+	public float getAsg2();
+	//get method to get marks of assignment 2
+
+	public void setExam(float mark);
+	//set marks in the exam
+
+	public float getExam();
+	//get exam marks
+
+	public float getTotal();//
+	//calculate total marks of student
 }
--- ITC515_assignment2/src/datamanagement/cgCTL.java
+++ ITC515_assignment2/src/datamanagement/cgCTL.java
@@ -28,7 +28,7 @@ public class cgCTL {
 	}
 
 	public void unitSelected(String code) {
-
+		//unit selected according to the selected code of the unit
 		if (code.equals("NONE"))
 			CGUI.setState2(false);
 		else {
@@ -54,7 +54,6 @@ public class cgCTL {
 			IStudent s = StudentManager.get().getStudent(id);
 
 			IStudentUnitRecord r = s.getUnitRecord(cuc);
-
 			CGUI.setRecord(r);
 			CGUI.setState3(true);
 			CGUI.setState4(true);
@@ -66,6 +65,7 @@ public class cgCTL {
 	}
 
 	public String checkGrade(float f, float g, float h) {
+		//get grades according to the marks
 		IUnit u = UnitManager.UM().getUnit(cuc);
 		String s = u.getGrade(f, g, h);
 		CGUI.setState4(true);
@@ -77,6 +77,7 @@ public class cgCTL {
 	}
 
 	public void enableChangeMarks() {
+		//change marks according to the selected unit and student
 		CGUI.setState4(false);
 		CGUI.setState6(false);
 		CGUI.setState5(true);
@@ -84,7 +85,7 @@ public class cgCTL {
 	}
 
 	public void saveGrade(float asg1, float asg2, float exam) {
-
+		//method to save grades according to unit and student selected
 		IUnit u = UnitManager.UM().getUnit(cuc);
 		IStudent s = StudentManager.get().getStudent(currentStudentID);
 
